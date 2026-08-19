b0298139f3b762c62a6b0dc852122acf5e5d1006
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2026-08-19 15:36:36 +0000

    Use mutexes for top.gg integration and format cat files

--- src/handlers/Cat.ts
+++ src/handlers/Cat.ts
@@ -1,5 +1,5 @@
-import { SlashRoute, SlashHandler } from "@seedcord/gateway";
-import { ContainerBuilder } from "discord.js";
+import { SlashRoute, SlashHandler } from "@seedcord/gateway"
+import { ContainerBuilder } from "discord.js"
 
 @SlashRoute("cat")
 export class Cat extends SlashHandler<"cat"> {
@@ -11,16 +11,16 @@ export class Cat extends SlashHandler<"cat"> {
         })
         await this.defer({ ephemeral: false })
         const cat = await catPromise
-        const catObject = await cat.json() as { tags: string[], url: string }
+        const catObject = (await cat.json()) as { tags: string[]; url: string }
         await this.edit({
-            components: [new ContainerBuilder()
-                .addTextDisplayComponents((builder) => builder
-                    .setContent(`### Tags: ${catObject.tags.join(", ")}`))
-                .addMediaGalleryComponents((builder) => builder
-                    .addItems((mediaGalleryItem) =>
-                        mediaGalleryItem
-                            .setDescription('A cat from CatAAS')
-                            .setURL(catObject.url)))
+            components: [
+                new ContainerBuilder()
+                    .addTextDisplayComponents((builder) => builder.setContent(`### Tags: ${catObject.tags.join(", ")}`))
+                    .addMediaGalleryComponents((builder) =>
+                        builder.addItems((mediaGalleryItem) =>
+                            mediaGalleryItem.setDescription("A cat from CatAAS").setURL(catObject.url)
+                        )
+                    )
             ]
         })
     }
