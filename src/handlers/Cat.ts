b5073a52f7324724f93ea39c61671f0a3462d4ea
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2026-08-19 15:09:38 +0000

    Add cat command

---
+++ src/handlers/Cat.ts
@@ -0,0 +1,27 @@
+import { SlashRoute, SlashHandler } from "@seedcord/gateway";
+import { ContainerBuilder } from "discord.js";
+
+@SlashRoute("cat")
+export class Cat extends SlashHandler<"cat"> {
+    public async execute(): Promise<void> {
+        const catPromise = fetch("https://cataas.com/cat", {
+            headers: {
+                Accept: "application/json"
+            }
+        })
+        await this.defer({ ephemeral: false })
+        const cat = await catPromise
+        const catObject = await cat.json() as { tags: string[], url: string }
+        await this.edit({
+            components: [new ContainerBuilder()
+                .addTextDisplayComponents((builder) => builder
+                    .setContent(`### Tags: ${catObject.tags.join(", ")}`))
+                .addMediaGalleryComponents((builder) => builder
+                    .addItems((mediaGalleryItem) =>
+                        mediaGalleryItem
+                            .setDescription('A cat from CatAAS')
+                            .setURL(catObject.url)))
+            ]
+        })
+    }
+}
