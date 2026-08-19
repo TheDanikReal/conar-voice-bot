6d45626a4486476e4b474351c8acc815910cc18b
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2026-08-19 15:09:38 +0000

    Add cat command

---
+++ src/commands/Cat.ts
@@ -0,0 +1,12 @@
+import { BuilderComponent, RegisterCommand } from "@seedcord/gateway"
+
+@RegisterCommand("global")
+export class Cat extends BuilderComponent<"command"> {
+    constructor() {
+        super("command")
+
+        this.instance
+            .setName("cat")
+            .setDescription("Get a random cat from CatAAS")
+    }
+}
